77858949bbc1dc21416a17ec3420ff971db61a56
olemoz1977 <olemoz1977@gmail.com>
2026-04-21 14:04:19 +0300

    Delete app.global-init.js

--- app.global-init.js
+++
@@ -1,5 +0,0 @@
-// app.global-init.js
-// Garantija, kad GLA globalas visada yra, net jei app.js nespėjo/nesugebėjo jo sukurti
-(function (global) {
-  if (!global.GLA) global.GLA = {};
-})(window);
