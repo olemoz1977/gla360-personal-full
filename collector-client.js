7560f1987713d9afb00eb1737b0f8c8f5bf56eff
olemoz1977 <olemoz1977@gmail.com>
2026-08-20 12:56:43 +0300

    Bust report enhancement cache

--- collector-client.js
+++ collector-client.js
@@ -117,7 +117,7 @@
     loadHelper('survey-enhancements.js?v=20260820-2');
   }
   if(/\/report-v2\.html$/i.test(location.pathname)){
-    loadHelper('report-coverage.js?v=20260820-1');
+    loadHelper('report-coverage.js?v=20260820-2');
   }
   if(/\/guardian\.html$/i.test(location.pathname)){
     loadHelper('guardian-invites.js?v=20260820-1');
