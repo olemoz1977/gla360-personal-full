44779e65c802919596c3c9236dbf40660ad166d9
olemoz1977 <olemoz1977@gmail.com>
2026-08-21 05:29:52 +0300

    Load safe comparison radar helper

--- collector-client.js
+++ collector-client.js
@@ -137,6 +137,9 @@
     loadHelper('report-plan-link.js?v=20260820-2');
     loadHelper('report-radar-missing-style.js?v=20260820-1');
   }
+  if(/\/compare-v2\.html$/i.test(location.pathname)){
+    loadHelper('compare-radar-safe.js?v=20260821-1');
+  }
   if(/\/guardian\.html$/i.test(location.pathname)){
     loadHelper('guardian-invites.js?v=20260820-2');
     loadHelper('guardian-workspace.js?v=20260820-1');
